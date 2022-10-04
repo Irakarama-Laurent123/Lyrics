54406ab9bdfffe3c2f2b04beb360a743eb11a799
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-10-04 11:37:20 +0200

    Added fetching data functionality

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1/",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "d14cd7572amsh009efd861bcd062p1ac1ffjsn092422be1171"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
