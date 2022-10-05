4c2728cbf564412eb044a545400a84fecb43c8a9
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-10-05 19:17:58 +0200

    Added the Top charts functionalities

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -12,9 +12,40 @@ import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
-const TopChartCard = ({ song, i }) => (
+const TopChartCard = ({
+  song,
+  i,
+  isPlaying,
+  activeSong,
+  handlePlayClick,
+  handlePauseClick,
+}) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-    {song.title}
+    <h3 className="font-bold text-base text-white mr-3 justify-between items-center">
+      {i + 1}.
+    </h3>
+    <div className="flex-1 flex flex-row items-center justify-between">
+      <img
+        src={song?.images.coverart}
+        alt={song?.title}
+        className="w-20 h-20 rounded-lg"
+      />
+      <div className=" flex-1 flex flex-col justify-center mx-3">
+        <Link to={`/songs/${song.key}`}>
+          <p className="text-xl font-bold text-white">{song?.title}</p>
+        </Link>
+        <Link to={`/artists/${song?.artists[0].adamid}`}>
+          <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
+        </Link>
+      </div>
+    </div>
+    <PlayPause
+      song={song}
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      handlePauseClick={handlePauseClick}
+      handlePlayClick={handlePlayClick}
+    />
   </div>
 );
 
@@ -34,7 +65,7 @@ const TopPlay = () => {
     dispatch(playPause(false));
   };
 
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
@@ -53,7 +84,15 @@ const TopPlay = () => {
         </div>
         <div className="mt-4 flex flex-col gap-1">
           {topPlays?.map((song, i) => (
-            <TopChartCard song={song} i={i} key={song.key} />
+            <TopChartCard
+              song={song}
+              i={i}
+              key={song.key}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => handlePlayClick(song, i)}
+            />
           ))}
         </div>
       </div>
