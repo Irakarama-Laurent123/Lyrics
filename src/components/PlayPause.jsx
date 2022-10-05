391537473b3fc189a2c531a7671e788b8a0c6a5c
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-10-05 08:57:38 +0200

    Added playing and pausing functionality

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable quotes */
 /* eslint-disable no-confusing-arrow */
-import { FaPauseCirlce, FaPlayCircle } from "react-icons";
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
 
 const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
   isPlaying && activeSong?.title === song.title ? (
-    <FaPauseCirlce size={35} className="text-gray-300 " onClick={handlePause} />
+    <FaPauseCircle size={35} className="text-gray-300 " onClick={handlePause} />
   ) : (
     <FaPlayCircle size={35} className="text-gray-300 " onClick={handlePlay} />
   );
